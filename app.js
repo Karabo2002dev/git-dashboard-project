a499f97cf0f61cc45b5e967fa2c4500f19522ba3
Karabo2002dev <kgapholakarabo01@gmail.com>
2025-09-13 23:08:42 +0200

    fix: display message for data fetching

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const API_ENDPOINT = "https://api.real-prod-api.com/data"
 const dataContainer = document.getElementById('data-container')
-const loadBtn = document.getElementById('load-data')
+const loadBtn = document.getElementById('load-data-btn')
 
 function fetchData() {
     dataContainer.innerHTML= `<span class="loading">Loading...</span>`
-    fetch(API_ENDPOINT).then(response => response.json()).then(data => {dataContainer.innerHTML = `<span class="loading"> Data Loaded: </span> ${data.message}`}).catch(error => {
+    fetch(API_ENDPOINT).then(response => response.json()).then(data => {dataContainer.innerHTML = `<strong> Data Loaded: </strong> ${data.message}`}).catch(error => {
         dataContainer.innerHTML = `<span class='error'>Failed to load data.</span>`
         console.error('Fetch Error:', error)
     })
